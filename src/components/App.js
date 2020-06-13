9f8d7b6df66a8ec132418ddad94d05069b93fb54
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2020-06-13 23:11:45 +0100

    Use Switch and Route

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "../styles/App.css";
 
 import NavBar from "./NavBar";
+import Properties from "./Properties";
+import AddProperty from "./AddProperty";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Switch>
+        <Route path="/" component={Properties} exact />
+        <Route path="/add-property" component={AddProperty} exact />
+      </Switch>
     </div>
   );
 }
