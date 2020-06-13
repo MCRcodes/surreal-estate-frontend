413725db0b8f115aa44dfb47c3fd3400d15255f1
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2020-06-13 23:32:41 +0100

    Add initialState, handleAddProperty and handleFieldChange plus styles

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -1,3 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/AddProperty.css";
 
-export default () => <>Add Property Page</>;
+const AddProperty = () => {
+  const initialState = {
+    fields: {
+      title: "",
+    },
+  };
+
+  const [fields, setFields] = useState(initialState.fields);
+
+  const handleAddProperty = (event) => {
+    event.preventDefault();
+    console.log(fields);
+  };
+
+  const handleFieldChange = (event) => {
+    const changedField = event.target.name;
+    const newValue = event.target.value;
+
+    setFields({ ...fields, [changedField]: newValue });
+  };
+
+  return (
+    <div className="add-property">
+      <form onSubmit={handleAddProperty}>
+        <label htmlFor="title">
+          Title
+          <input
+            id="title"
+            name="title"
+            value={fields.title}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddProperty;
