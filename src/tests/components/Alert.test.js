3da513e0ed8e2b4a3bc67c48e1a4c48baaf7292b
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2020-06-22 17:05:51 +0100

    Add extra test to Alert

--- src/tests/components/Alert.test.js
+++ src/tests/components/Alert.test.js
@@ -5,8 +5,8 @@ import Alert from "../../components/Alert";
 test("displays an error message", () => {
   const { getByText, asFragment } = render(<Alert message="Error!" />);
 
-  expect(asFragment()).toMatchSnapshot();
   expect(getByText(/Error/).textContent).toBe("Error!");
+  expect(asFragment()).toMatchSnapshot();
 });
 
 test("displays a success message", () => {
@@ -14,6 +14,12 @@ test("displays a success message", () => {
     <Alert message="Success!!!!" success />
   );
 
-  expect(asFragment()).toMatchSnapshot();
   expect(getByText(/Success/).textContent).toBe("Success!!!!");
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test("does not render an error or a success message if message props is empty", () => {
+  const { asFragment } = render(<Alert message="" />);
+
+  expect(asFragment()).toMatchSnapshot();
 });
