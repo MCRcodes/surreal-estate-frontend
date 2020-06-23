0b148dc155cd054691651f4ebde62a86d8fd45ae
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2020-06-24 00:50:12 +0100

    Add Properties, PropertyCard and Sidebar

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,3 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Proptypes from "prop-types";
+import PropertyCard from "./PropertyCard";
+import Alert from "./Alert";
+import Sidebar from "./SideBar";
+import { getProperties, filterByCity } from "../requests";
 
-export default () => <>Properties Page</>;
+import "../styles/Properties.css";
+
+const Properties = ({ location }) => {
+  const [properties, setProperties] = useState([]);
+  const [alert, setAlert] = useState({ message: "", isSuccess: false });
+  useEffect(() => {
+    getProperties(setProperties, setAlert);
+  }, []);
+
+  const { search } = location;
+  const [currentCity, setCity] = useState(search);
+  useEffect(() => {
+    const decodedCurrentCity = decodeURIComponent(currentCity);
+    if (search !== decodedCurrentCity) {
+      filterByCity(search, setCity, setProperties, setAlert);
+      // axios
+      //   .get(`http://localhost:4000/api/v1/PropertyListing${search}`)
+      //   .then(({ data }) => {
+      //     if (data.length === 0) {
+      //       setAlert({ message: "No properties found", isSuccess: true });
+      //       setProperties(data);
+      //     } else {
+      //       setAlert({ message: "", isSuccess: false });
+      //       setCity(search);
+      //       setProperties(data);
+      //     }
+      //   })
+      //   .catch(() =>
+      //     setAlert({ message: "Server error. Please try again later." })
+      //   );
+    }
+  }, [search, currentCity]);
+
+  return (
+    <div className="Properties">
+      <Sidebar />
+      <Alert message={alert.message} success={alert.isSuccess} />
+      {properties.map((property) => (
+        <PropertyCard key={property._id} {...property} />
+      ))}
+    </div>
+  );
+};
+
+Properties.propTypes = {
+  location: Proptypes.shape({
+    search: Proptypes.string,
+  }).isRequired,
+};
+
+export default Properties;
