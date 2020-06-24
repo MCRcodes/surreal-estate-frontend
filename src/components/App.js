83eb1836e3174d75e541fac9cc211a60ea36bdca
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2020-06-24 23:14:06 +0100

    Add facebook SSO

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import "../styles/App.css";
 
@@ -6,16 +6,24 @@ import NavBar from "./NavBar";
 import Properties from "./Properties";
 import AddProperty from "./AddProperty";
 
-function App() {
+const App = () => {
+  const [userID, setUserID] = useState("");
+
+  const handleLogin = (response) => {
+    setUserID(response.userID);
+  };
+
+  const handleLogout = () => window.FB.logout(() => setUserID(""));
+
   return (
     <div className="App">
-      <NavBar />
+      <NavBar onLogin={handleLogin} onLogout={handleLogout} userID={userID} />
       <Switch>
         <Route path="/" component={Properties} exact />
         <Route path="/add-property" component={AddProperty} exact />
       </Switch>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,52 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import FacebookLogin from "react-facebook-login";
 import "../styles/NavBar.css";
 
-const responseFacebook = (response) => {
-  console.log(response);
-};
+const LogoutButton = ({ onLogout }) => (
+  <button type="button" onClick={onLogout}>
+    Logout
+  </button>
+);
 
-function NavBar() {
-  return (
-    <div className="navbar">
-      <img
-        className="navbar-logo"
-        src="https://mcrcodes.s3.eu-west-2.amazonaws.com/course/surreal-estate/logo.png"
-        alt="logo"
-      />
-      <ul className="navbar-links">
-        <li className="navbar-links-item">
-          <Link to="/">View Properties</Link>
-        </li>
-        <li className="navbar-links-item">
-          <Link to="/add-property">Add Property</Link>
-        </li>
+const NavBar = ({ onLogin, onLogout, userID }) => (
+  <div className="navbar">
+    <img
+      className="navbar-logo"
+      src="https://mcrcodes.s3.eu-west-2.amazonaws.com/course/surreal-estate/logo.png"
+      alt="logo"
+    />
+    <ul className="navbar-links">
+      <li className="navbar-links-item">
+        <Link to="/">View Properties</Link>
+      </li>
+      <li className="navbar-links-item">
+        <Link to="/add-property">Add Property</Link>
+      </li>
+      {!userID ? (
         <FacebookLogin
           appId="3048483165266445"
           autoLoad
           fields="name,email,picture"
           // onClick={componentClicked}
-          callback={responseFacebook}
+          callback={onLogin}
         />
-      </ul>
-    </div>
-  );
-}
+      ) : (
+        <LogoutButton onLogout={onLogout} />
+      )}
+    </ul>
+  </div>
+);
+
+LogoutButton.propTypes = {
+  onLogout: PropTypes.func.isRequired,
+};
+
+NavBar.propTypes = {
+  onLogin: PropTypes.func.isRequired,
+  onLogout: PropTypes.func.isRequired,
+  userID: PropTypes.string.isRequired,
+};
 
 export default NavBar;
